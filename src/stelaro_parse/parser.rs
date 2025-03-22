04d760cfed7114d2308e2b99e25327abd8c341cd
stellanomia <fulmlumo@gmail.com>
2025-03-22 22:34:26 +0900

    parse_ident の実装、診断の追加

--- src/stelaro_parse/parser.rs
+++ src/stelaro_parse/parser.rs
@@ -1,9 +1,11 @@
-use crate::stelaro_ast::token::{Token, TokenKind, TokenStream};
+use crate::stelaro_ast::token::{Lit, LiteralKind, Token, TokenKind, TokenStream};
 use crate::stelaro_common::span::Span;
+use crate::stelaro_common::symbol::Ident;
 use crate::stelaro_diagnostic::diag::{DiagCtxtHandle, ErrorEmitted};
 use crate::stelaro_session::Session;
 
 use super::diagnostics::DiagsParser;
+use super::PResult;
 
 
 pub struct Parser<'sess> {
@@ -57,4 +59,40 @@ impl<'sess> Parser<'sess> {
         }
     }
 
+    pub fn look_ahead(&self, k: usize) -> Option<Token> {
+        self.token_stream.peek_nth(k).cloned()
+    }
+
+    pub fn parse_ident(&mut self) -> PResult<Ident> {
+
+        if let TokenKind::Ident(symbol) = self.token.kind {
+            self.bump();
+
+            Ok(Ident::new(symbol, self.prev_token.span))
+        } else if let TokenKind::Literal(lit @ Lit {
+            kind: LiteralKind::Integer | LiteralKind::Float, ..
+        }) = self.token.kind {
+                let next = self.look_ahead(1);
+                let span = if let Some(TokenKind::Ident(_)) = next.map(|t| t.kind) {
+                    self.token.span.merge(&next.unwrap().span)
+                } else {
+                    self.token.span
+                };
+
+                Err(
+                    DiagsParser::unexpected_numeric_literal_for_identifier(
+                        self.dcx(),
+                        lit,
+                        span,
+                    ).emit()
+                )?
+        } else {
+            Err(
+                DiagsParser::unexpected_token_for_identifier(
+                    self.dcx(),
+                    self.token.span,
+                ).emit()
+            )?
+        }
+    }
 }
--- src/stelaro_parse/stmt.rs
+++ src/stelaro_parse/stmt.rs
@@ -7,8 +7,10 @@ impl Parser<'_> {
     pub fn parse_stmt(&mut self) -> PResult<Stmt> {
         match self.token.kind {
             TokenKind::Let => {
-                self.bump();
-                todo!()
+                self.parse_let()
+            }
+            TokenKind::If => {
+                self.parse_if()
             }
             _ => {
                 todo!()
@@ -17,6 +19,18 @@ impl Parser<'_> {
     }
 
     pub fn parse_block(&mut self) -> PResult<Block> {
+        todo!()
+    }
+
+    pub fn parse_let(&mut self) -> PResult<Stmt> {
+        self.eat(TokenKind::Let, self.token.span)?;
+
+        todo!()
+    }
+
+    pub fn parse_if(&mut self) -> PResult<Stmt> {
+        self.eat(TokenKind::If, self.token.span)?;
+
         todo!()
     }
 }
