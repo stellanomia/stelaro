7819615aa8478ab8f7982411ce6e7c795aeebd02
fulm-o <fulmlumo@gmail.com>
2025-05-04 21:22:27 +0900

    DefPathHash, StableSteloId の実装

--- src/stelaro_common/def_id.rs
+++ src/stelaro_common/def_id.rs
@@ -1,6 +1,7 @@
-use std::fmt;
+//! rustc の `rustc_span/def_id.rs` に基づいて設計されています。
 
-use super::Idx;
+use std::{fmt, hash::Hash};
+use super::{fingerprint::Fingerprint, Hash64, Idx, Symbol, StableHasher};
 
 // NOTE: std, core 実装まで使われない
 /// stelo を一意に識別する番号。
@@ -46,6 +47,105 @@ impl From<SteloNum> for u32 {
     }
 }
 
+/// `DefPathHash` は `DefPath` を固定長ハッシュで表現した構造体です。
+/// これは2つの独立した64ビットハッシュから構成されます。
+/// 最初のハッシュは、この `DefPathHash` が由来するクレートを一意に識別し
+/// ([`StableSteloId`] を参照)、次のハッシュはそのクレート内の対応する `DefPath` を
+/// 一意に識別します。これらを組み合わせることで、クレートグラフ全体で一意な識別子を形成します。
+/// 高品質なハッシュ関数を使用すれば、ハッシュの衝突確率は
+/// 非常に低いですが、該当するアイテムの名前を変更したり
+/// するなどで簡単に回避することができます。
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct DefPathHash(pub Fingerprint);
+
+
+impl DefPathHash {
+    /// [DefPathHash] の由来元であるステロを示す [StableSteloId] を返します。
+    #[inline]
+    pub fn stable_stelo_id(&self) -> StableSteloId {
+        StableSteloId(self.0.split().0)
+    }
+
+    /// [DefPathHash] のうち、ステロにローカルな部分を返します。
+    #[inline]
+    pub fn local_hash(&self) -> Hash64 {
+        self.0.split().1
+    }
+
+    /// 指定された [StableSteloId] と `local_hash` を用いて、新しい [DefPathHash] を構築します。
+    /// ただし、`local_hash` はそのクレート内で一意でなければなりません。
+    pub fn new(stable_stelo_id: StableSteloId, local_hash: Hash64) -> DefPathHash {
+        DefPathHash(Fingerprint::new(stable_stelo_id.0, local_hash))
+    }
+}
+
+
+/// [`StableSteloId`] は、ステロ名やその他のいくつかのデータを
+/// 組み合わせた64ビットのハッシュ値です。[`DefPathHash`] が [`DefId`] に対応するのと同様に、
+/// [`SteloNum`] に対応するものです。このIDはコンパイルセッション間で安定しています。
+///
+/// このIDはハッシュ値であるため、2つの異なるステロが偶然同じ [`StableSteloId`] を持つ可能性が
+/// わずかにあります。コンパイラはステロの読み込み時にそのような衝突を検出し、
+/// さらなる問題を防ぐためにコンパイルを中断します。
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct StableSteloId(pub Hash64);
+
+impl StableSteloId {
+    pub fn new(
+        stelo_name: Symbol,
+        // 将来的に -Cmetadata のようなビルド固有の識別情報を受け取る場合に備える
+        // mut metadata: Vec<String>,
+        // 将来的に言語/コンパイラバージョンの違いをハッシュに反映させたい場合に備える
+        // cfg_version: &'static str,
+    ) -> StableSteloId {
+        let mut hasher = StableHasher::new();
+
+        // stelo_name の内部IDはビルドごとに変わる可能性があるため、
+        // ハッシュには安定している文字列表現を使う
+        stelo_name.as_str().hash(&mut hasher);
+
+        // // -Cmetadata の引数の順序に依存しないようにソート
+        // metadata.sort();
+        // // 同じ値が複数ある場合は一度だけ反映する
+        // metadata.dedup();
+
+        // hasher.write(b"metadata");
+        // for s in &metadata {
+        //     // `-Cmetadata=ab -Cmetadata=c` と `-Cmetadata=a -Cmetadata=bc` のような
+        //     // 紛らわしいケースの区別のため、文字列長も含めてハッシュ化
+        //     hasher.write_usize(s.len());
+        //     hasher.write(s.as_bytes());
+        // }
+
+        // // コンパイラバージョンによってシンボルが衝突しないようにするため、
+        // // 明示的にバージョン文字列を含める（rustc と同様）
+        // if let Some(val) = std::env::var_os("STELARO_FORCE_STELARO_VERSION") {
+        //     hasher.write(val.to_string_lossy().into_owned().as_bytes())
+        // } else {
+        //     hasher.write(cfg_version.as_bytes())
+        // }
+
+        StableSteloId(hasher.finish())
+    }
+
+    #[inline]
+    pub fn as_u64(self) -> u64 {
+        self.0.as_u64()
+    }
+}
+
+impl fmt::LowerHex for StableSteloId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::LowerHex::fmt(&self.0, f)
+    }
+}
+
+impl Default for DefPathHash {
+    fn default() -> Self {
+        DefPathHash(Fingerprint::ZERO)
+    }
+}
+
 
 /// ステロ内の定義を一意に識別するインデックス。
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
@@ -84,6 +184,19 @@ impl From<DefIndex> for u32 {
         value.0
     }
 }
+
+impl From<usize> for DefIndex {
+    fn from(value: usize) -> Self {
+        DefIndex(value as u32)
+    }
+}
+
+impl From<u32> for DefIndex {
+    fn from(value: u32) -> Self {
+        DefIndex(value)
+    }
+}
+
 
 
 /// コンパイラ内の全ての定義 (ローカル、外部) を一意に識別するID。
