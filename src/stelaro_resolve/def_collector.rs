5f5346919c0325dcee1d5071c4f8ce15d528efe9
stellanomia <fulmlumo@gmail.com>
2025-05-07 14:36:36 +0900

    collect_definitions の実装

--- src/stelaro_resolve/def_collector.rs
+++ src/stelaro_resolve/def_collector.rs
@@ -6,7 +6,15 @@ use crate::stelaro_sir::def::DefKind;
 
 use super::Resolver;
 
-// TODO: 定義を集め、node_id_to_def_id に定義を登録しつつ、create_def() していく
+pub fn collect_definitions(
+    resolver: &mut Resolver<'_, '_>,
+    stelo: &Stelo,
+) {
+    let parent_def = STELO_DEF_ID;
+    let mut visitor = DefCollector { resolver, parent_def};
+    visitor.visit_stelo(stelo);
+}
+
 struct DefCollector<'a, 'ra, 'tcx> {
     resolver: &'a mut Resolver<'ra, 'tcx>,
     parent_def: LocalDefId,
@@ -38,7 +46,6 @@ impl<'a, 'ra, 'tcx> DefCollector<'a, 'ra, 'tcx> {
 
 impl<'a, 'ra, 'tcx> Visitor<'a> for DefCollector<'a, 'ra, 'tcx> {
     fn visit_item(&mut self, item: &'a Item) {
-        let parent_def = self.parent_def;
         let def_kind = match &item.kind {
             ItemKind::Fn(..) => DefKind::Fn,
             ItemKind::Mod(..) => DefKind::Mod,
