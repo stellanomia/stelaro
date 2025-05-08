15acb4c3b27b4d93036a397bd26f73c0402a9619
fulm-o <fulmlumo@gmail.com>
2025-05-08 21:10:59 +0900

    report_conflict の仮実装

--- src/stelaro_resolve/diagnostics.rs
+++ src/stelaro_resolve/diagnostics.rs
@@ -2,7 +2,7 @@ use crate::stelaro_common::Ident;
 use crate::stelaro_diagnostic::diag::DiagCtxtHandle;
 use crate::stelaro_sir::def::Namespace;
 
-use super::{Module, NameBinding, Resolver};
+use super::{Module, ModuleKind, NameBinding, Resolver};
 
 
 impl<'ra, 'tcx> Resolver<'ra, 'tcx> {
@@ -18,6 +18,17 @@ impl<'ra, 'tcx> Resolver<'ra, 'tcx> {
         new_binding: NameBinding<'ra>,
         old_binding: NameBinding<'ra>,
     ) {
-        
+        // 名前衝突が起こった、二個目の名前に対して診断する
+        if old_binding.span.start > new_binding.span.start {
+            // new_binding, old_binding を入れ替える
+            self.report_conflict(parent, ident, ns, old_binding, new_binding);
+        }
+
+        let container = match parent.kind {
+            ModuleKind::Def(kind, _, _) => kind.descr(parent.def_id()),
+            ModuleKind::Block => "block",
+        };
+
+
     }
 }
