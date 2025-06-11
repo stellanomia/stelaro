5dd666c8a8c389cd1e227ac683e50c42ac3647e4
stellanomia <fulmlumo@gmail.com>
2025-06-11 15:37:21 +0900

    関数のブロックに対し resolve_block が呼び出されないバグ修正

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,9 @@ use stelaro::temp;
 fn main() {
 
 temp(r#"
-fn main(x: _, x: _) {
-    -x * y
-}
-
-mod my_mod {
-    fn f(): i32 {
-        let x = if 123 < 456 {
-            789
-        } else {
-            0
-        };
-
-        x
-    }
-
-    fn a(x: i32) {
-        print("Hello");
-    }
+fn main(x: _, y: _, z: _) {
+    let x;
+    let z;
 }
 "#.trim().to_string());
 }
