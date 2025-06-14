d06cc42a904cbaf5357be9c1b9d56a2da583cec0
fulm-o <fulmlumo@gmail.com>
2025-06-14 12:15:02 +0900

    resolve_ident_in_lexical_scope の実装

--- src/stelaro_resolve/ident.rs
+++ src/stelaro_resolve/ident.rs
@@ -1,8 +1,10 @@
 use crate::stelaro_common::Ident;
-use crate::stelaro_resolve::{Determinacy, Finalize, LexicalScopeBinding, PathResult, Segment};
+use crate::stelaro_resolve::{
+    Determinacy, Finalize, LexicalScopeBinding, ModuleKind,
+    PathResult, Segment, late::{Scope, ScopeKind}, Module,
+    NameBinding, Resolver
+};
 use crate::stelaro_sir::def::{Namespace::{self, ValueNS, TypeNS}, PerNS};
-
-use super::{late::Scope, Module, NameBinding, Resolver};
 
 
 impl<'ra, 'tcx> Resolver<'ra, 'tcx> {
@@ -18,6 +20,19 @@ impl<'ra, 'tcx> Resolver<'ra, 'tcx> {
         todo!()
     }
 
+    /// これは、現在のレキシカルスコープ内で、名前空間 `ns` の識別子 `ident` を解決します。
+    /// より具体的には、スコープの階層を上にたどり、`ident` を定義している最初のスコープでの
+    /// 束縛を返します (どのスコープもそれを定義していない場合は `None` を返します)。
+    ///
+    /// ブロックのアイテムは、そのアイテムがブロック内のどこで定義されているかに関わらず、
+    /// スコープ階層において、そのローカル変数よりも上にあります。例えば、
+    /// ```
+    /// fn f() {
+    ///    g(); // スコープ内にまだローカル変数がないため、これはアイテムを参照するように解決されます。
+    ///    let g = 1;
+    ///    g + g; // これはアイテムをシャドーイングするため、ローカル変数 `g` に解決されます。
+    /// }
+    /// ```
     pub fn resolve_ident_in_lexical_scope(
         &mut self,
         ident: Ident,
@@ -27,7 +42,51 @@ impl<'ra, 'tcx> Resolver<'ra, 'tcx> {
         scopes: &[Scope<'ra>],
         ignore_binding: Option<NameBinding<'ra>>,
     ) -> Option<LexicalScopeBinding<'ra>> {
-        todo!()
+
+        // スコープスタックを逆順に辿る
+        let mut module = self.graph_root;
+        for scope in scopes.iter().rev() {
+            if let Some(res) = scope.bindings.get(&ident) {
+                // 識別子は型パラメータまたはローカル変数に解決される。
+                return Some(
+                    LexicalScopeBinding::Res(
+                        *res
+                    )
+                );
+            }
+
+            module = match scope.kind {
+                ScopeKind::Module(module) => module,
+                _ => continue,
+            };
+
+            match module.kind {
+                ModuleKind::Block => {}, // ブロックは通過して見ることができる
+                _ => break,
+            }
+
+            let item = self.resolve_ident_in_module(
+                &module,
+                ident,
+                ns,
+                parent_module,
+                finalize,
+                ignore_binding
+            );
+
+            if let Ok(binding) = item {
+                return Some(LexicalScopeBinding::Item(binding));
+            }
+        }
+
+        self.resolve_ident_in_ambience(
+            ident,
+            parent_module,
+            ns,
+            finalize
+        )
+        .ok()
+        .map(LexicalScopeBinding::Item)
     }
 
     pub fn resolve_ident_in_ambience(
